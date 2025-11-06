fe5cb9b8a601e7d316a4976044c1152fb05db2f6
reda guemry <guemryreda@gmail.com>
2025-11-07 00:25:10 +0100

    add the js of panier and give hem somme animations

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 const iconburgermeu = document.getElementById("nav__burgermnuicon");
 const navlist = document.getElementById("nav__listul")
 const animburgermenu = document.getElementById("burgermenu__anim")
+const iconpanier = document.getElementById("navpanier__logo")
+const paniersection = document.getElementById("opensection__panierpage")
+const paniersectionclose = document.getElementById("closesection__panierpage")
 
 
-iconburgermeu.addEventListener("click" , (e) =>{
+iconburgermeu.addEventListener("click" , () =>{
     navlist.classList.toggle("hidden")
     animburgermenu.classList.toggle("w-full")
 })
+
+iconpanier.addEventListener("click" , () => {
+    paniersection.classList.remove("-right-full"); 
+    paniersection.classList.add("right-0")
+})
+
+paniersectionclose.addEventListener("click" , () => {
+    paniersection.classList.add("-right-full"); 
+    paniersection.classList.remove("right-0")
+})
+
+
