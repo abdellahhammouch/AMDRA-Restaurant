f96c4c5b248beb0a0b45fe12294d1a5b96c6e1f0
reda guemry <guemryreda@gmail.com>
2025-11-07 00:00:37 +0100

    add the js of burger menu and animations

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-
 const iconburgermeu = document.getElementById("nav__burgermnuicon");
- 
+const navlist = document.getElementById("nav__listul")
+const animburgermenu = document.getElementById("burgermenu__anim")
 
+
+iconburgermeu.addEventListener("click" , (e) =>{
+    navlist.classList.toggle("hidden")
+    animburgermenu.classList.toggle("w-full")
+})
