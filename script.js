115be76c7d52c522c7cbf5b2516b496fa2333962
reda guemry <guemryreda@gmail.com>
2025-11-11 14:31:15 +0100

    add the affichage of page detaillse

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ paniersectionclose.addEventListener("click" , () => {
 
 let plat ;
 
-fetch("/data.json")
+fetch("/src/plats.json")
     .then(response  => {
         if(!response.ok){
             throw new Error("Erreur dans le chargement du fichier JSON");
@@ -52,133 +52,230 @@ fetch("/data.json")
     .catch(error => {console.error(error)})
 
 
-
-    
-
-
-document.querySelectorAll(".pourDetails").forEach(el => {
-     el.addEventListener("click" , (e) => {
-
-        const id = e.currentTarget.dataset.id;
-        localStorage.setItem("courrentIdDeyails" , id );
-
-        location.href = "/src/pages/detail-menu.html";
-
-    })
-});
-
 function afficherDetails() {
-
+    console.log("heeey");
+    
     console.log();
-    const courrentId = localStorage.getItem("courrentIdDeyails");
+    const courrentId = localStorage.getItem("courrentIdDetails");
+    const courrentCategorie = localStorage.getItem("courrentcategorieDetails");
 
-    for(ele of plat.dishes){
-        if(ele.id == courrentId) {
-            console.log(ele);
-            const detailsele = document.createElement("section")
-            let appn = document.getElementById("opensection__panierpage")
-            detailsele.setAttribute("class" , "mt-28")
-            detailsele.innerHTML = ` 
-            <div class="w-[95%] md:w-[80%] mx-auto flex flex-col md:flex-row gap-7 bg-orange-50 rounded-3xl p-5">
-
-              <div class="w-full md:w-1/2 ">
-
-                <div class="w-full">
-                    <img src="../images/plat-test.png" alt="" class="rounded-2xl bg-[#F7EDE1]">
-                </div>
-
-                
-                <div class=" grid grid-cols-4 mt-3.5 w-full justify-items-center">
-
-                    <div class="max-w-20 h-20 bg-white border-2 border-orange-500 rounded-xl">
-                    <img src="../images/plat-test.png" alt="" class="">
+    if(courrentCategorie == "dishes"){        
+        for(ele of plat.dishes){
+            if(ele.id == courrentId) {
+                console.log(ele);
+                const detailsele = document.createElement("section")
+                let appn = document.querySelector("nav")
+                detailsele.setAttribute("class" , "mt-28")
+                detailsele.innerHTML = ` 
+                <div class="w-[95%] md:w-[80%] mx-auto flex flex-col md:flex-row gap-7 bg-orange-50 rounded-3xl p-5">
+    
+                  <div class="w-full md:w-1/2 ">
+    
+                    <div class="w-full">
+                        <img src="../${ele.image}" alt="" class="rounded-2xl  bg-[#F7EDE1]">
                     </div>
-
-                    <div class="max-w-20 h-20 bg-white border-2 border-gray-200 rounded-xl">
-                    <img src="../images/plat-test.png" alt="" class="">
-                    </div>
-
-                    <div class="max-w-20 h-20 bg-white border-2 border-gray-200 rounded-xl">
-                    <img src="../images/plat-test.png" alt="" class="">
-                    </div>
-
-                    <div class="max-w-20 h-20 bg-white border-2 border-gray-200 rounded-xl">
-                    <img src="../images/plat-test.png" alt="" class="">
-                    </div>
+    
                     
-                </div>
-
-                </div>
-
-                <div class="md:w-1/2  ">
-
-                <div class="">
-                    <h3 class="text-7xl text-black font-bold mb-2">Tajine Royal</h3>
-                    <p class="text-orange-500 text-xl pb-3 pl-1 border-b border-gray-300">85 DH</p>
-                </div>
-
-                <div class="pl-1 border-b border-gray-300 ">
-                    <p class="text-2xl p-1.5 ">Un tajine marocain authentique préparé avec des morceaux tendres d'agneau, des pruneaux moelleux, des amandes grillées et un mélange d'épices traditionnel. Mijoté lentement selon la recette ancestrale de nos grands-mères pour une saveur incomparable.</p>
-                </div>
-
-                <div class="flex gap-3 items-center  pl-1 border-b border-gray-300 ">
-                    <p class="font-bold">ingredients</p>
-                    <div class="size-12 grid items-center">
-                    <img src="../images/Ananas.png" alt="" class="w-10/12 rounded border ">
+                    <div class=" grid grid-cols-4 mt-3.5 w-full justify-items-center">
+    
+                        <div class="max-w-20 h-20 bg-white border-2 border-gray-200 rounded-xl">
+                        <img src="../${ele.image}" alt="" class="">
+                        </div>
+    
+                        <div class="max-w-20 h-20 bg-white border-2 border-gray-200 rounded-xl">
+                        <img src="../${ele.image}" alt="" class="">
+                        </div>
+    
+                        <div class="max-w-20 h-20 bg-white border-2 border-gray-200 rounded-xl">
+                        <img src="../${ele.image}" alt="" class="">
+                        </div>
+    
+                        <div class="max-w-20 h-20 bg-white border-2 border-gray-200 rounded-xl">
+                        <img src="../${ele.image}" alt="" class="">
+                        </div>
+                        
                     </div>
-                    <div class="size-12 grid items-center">
-                    <img src="../images/Ananas.png" alt="" class="w-10/12 rounded border">
+    
                     </div>
-                    <div class="size-12 grid items-center">
-                    <img src="../images/Ananas.png" alt="" class="w-10/12 rounded border">
-                    </div>
-                    
-                </div>
-
-                <div class="py-2  flex justify-between items-center">
-                    <span class="font-bold py-2">Size</span>
-
+    
+                    <div class="md:w-1/2  ">
+    
                     <div class="">
-                    <button class="px-2 py-0.5 rounded-md border bg-black text-white ">S</button>
-                    <button class="px-2 py-0.5 rounded-md border ">M</button>
-                    <button class="px-2 py-0.5 rounded-md border ">L</button>
-                    
+                        <h3 class="text-7xl text-black font-bold mb-2">${ele.name}</h3>
+                        <p class="text-orange-500 text-xl pb-3 pl-1 border-b border-gray-300">${ele.price}$</p>
                     </div>
-
-                </div>
-
-                <div class="">
-
-                    <div class="flex justify-between border-b border-gray-300">
-                    <span class="font-bold ">Total Price</span>
-                    <span class="font-semibold">200 DH</span>
+    
+                    <div class="pl-1 border-b border-gray-300 ">
+                        <p class="text-2xl p-1.5 ">${ele.description}.</p>
                     </div>
-
-                    <div class="md:grid grid-cols-[auto_1fr] gap-2 mt-2.5">
-
-                    <div class="flex justify-between items-center border-2 border-gray-200 rounded-4xl">
-                        <button class="px-3 py-1 text-gray-600 hover:text-gray-900 ">-</button>
-                        <span class="px-3 py-1 font-semibold">1</span>
-                        <button class="px-3 py-1 text-gray-600 hover:text-gray-900">+</button>
+    
+                    <div class="flex gap-3 items-center  pl-1 border-b border-gray-300 ">
+                        <p class="font-bold">ingredients</p>
+                        <div class="size-12 grid items-center">
+                        <img src="../${ele.ingredients[0].image}" alt="" class="w-10/12 rounded border ">
+                        </div>
+                        <div class="size-12 grid items-center">
+                        <img src="../${ele.ingredients[1].image}" alt="" class="w-10/12 rounded border">
+                        </div>
+                        <div class="size-12 grid items-center">
+                        <img src="../${ele.ingredients[2].image}" alt="" class="w-10/12 rounded border">
+                        </div>
+                        
                     </div>
-
+    
+                    <div class="py-2  flex justify-between items-center">
+                        <span class="font-bold py-2">Size</span>
+    
+                        <div class="">
+                        <button class="px-2 py-0.5 rounded-md border bg-black text-white ">S</button>
+                        <button class="px-2 py-0.5 rounded-md border ">M</button>
+                        <button class="px-2 py-0.5 rounded-md border ">L</button>
+                        
+                        </div>
+    
+                    </div>
+    
                     <div class="">
-                        <button class="w-full bg-amber-600 text-center p-1.5 rounded-4xl text-white ">Add to cart</button>
+    
+                        <div class="flex justify-between border-b border-gray-300">
+                        <span class="font-bold ">Total Price</span>
+                        <span class="font-semibold">200 DH</span>
+                        </div>
+    
+                        <div class="md:grid grid-cols-[auto_1fr] gap-2 mt-2.5">
+    
+                        <div class="flex justify-between items-center border-2 border-gray-200 rounded-4xl">
+                            <button class="px-3 py-1 text-gray-600 hover:text-gray-900 ">-</button>
+                            <span class="px-3 py-1 font-semibold">1</span>
+                            <button class="px-3 py-1 text-gray-600 hover:text-gray-900">+</button>
+                        </div>
+    
+                        <div class="">
+                            <button class="w-full bg-amber-600 text-center p-1.5 rounded-4xl text-white ">Add to cart</button>
+                        </div>
+                        </div>
+    
                     </div>
-                    </div>
-
+    
+                    </div> 
+                    
                 </div>
-
-                </div> 
-                
-            </div>
-            `
-            appn.after(detailsele);
-
+                `
+                appn.after(detailsele);
+    
+            }
+        }
+    }else if(courrentCategorie == "promotions"){
+        console.log("rah dkhalt lpromo");
+        for(ele of plat.promotions){
+            if(ele.id == courrentId) {
+                console.log(ele);
+                const detailsele = document.createElement("section")
+                let appn = document.querySelector("nav")
+                detailsele.setAttribute("class" , "mt-28")
+                detailsele.innerHTML = ` 
+                <div class="w-[95%] md:w-[80%] mx-auto flex flex-col md:flex-row gap-7 bg-orange-50 rounded-3xl p-5">
+    
+                  <div class="w-full md:w-1/2 ">
+    
+                    <div class="w-full">
+                        <img src="../images/plat-test.png" alt="" class="rounded-2xl bg-[#F7EDE1]">
+                    </div>
+    
+                    
+                    <div class=" grid grid-cols-4 mt-3.5 w-full justify-items-center">
+    
+                        <div class="max-w-20 h-20 bg-white border-2 border-orange-500 rounded-xl">
+                        <img src="../images/plat-test.png" alt="" class="">
+                        </div>
+    
+                        <div class="max-w-20 h-20 bg-white border-2 border-gray-200 rounded-xl">
+                        <img src="../images/plat-test.png" alt="" class="">
+                        </div>
+    
+                        <div class="max-w-20 h-20 bg-white border-2 border-gray-200 rounded-xl">
+                        <img src="../images/plat-test.png" alt="" class="">
+                        </div>
+    
+                        <div class="max-w-20 h-20 bg-white border-2 border-gray-200 rounded-xl">
+                        <img src="../images/plat-test.png" alt="" class="">
+                        </div>
+                        
+                    </div>
+    
+                    </div>
+    
+                    <div class="md:w-1/2  ">
+    
+                    <div class="">
+                        <h3 class="text-7xl text-black font-bold mb-2">Tajine Royal</h3>
+                        <p class="text-orange-500 text-xl pb-3 pl-1 border-b border-gray-300">85 DH</p>
+                    </div>
+    
+                    <div class="pl-1 border-b border-gray-300 ">
+                        <p class="text-2xl p-1.5 ">Un tajine marocain authentique préparé avec des morceaux tendres d'agneau, des pruneaux moelleux, des amandes grillées et un mélange d'épices traditionnel. Mijoté lentement selon la recette ancestrale de nos grands-mères pour une saveur incomparable.</p>
+                    </div>
+    
+                    <div class="flex gap-3 items-center  pl-1 border-b border-gray-300 ">
+                        <p class="font-bold">ingredients</p>
+                        <div class="size-12 grid items-center">
+                        <img src="../images/Ananas.png" alt="" class="w-10/12 rounded border ">
+                        </div>
+                        <div class="size-12 grid items-center">
+                        <img src="../images/Ananas.png" alt="" class="w-10/12 rounded border">
+                        </div>
+                        <div class="size-12 grid items-center">
+                        <img src="../images/Ananas.png" alt="" class="w-10/12 rounded border">
+                        </div>
+                        
+                    </div>
+    
+                    <div class="py-2  flex justify-between items-center">
+                        <span class="font-bold py-2">Size</span>
+    
+                        <div class="">
+                        <button class="px-2 py-0.5 rounded-md border bg-black text-white ">S</button>
+                        <button class="px-2 py-0.5 rounded-md border ">M</button>
+                        <button class="px-2 py-0.5 rounded-md border ">L</button>
+                        
+                        </div>
+    
+                    </div>
+    
+                    <div class="">
+    
+                        <div class="flex justify-between border-b border-gray-300">
+                        <span class="font-bold ">Total Price</span>
+                        <span class="font-semibold">200 DH</span>
+                        </div>
+    
+                        <div class="md:grid grid-cols-[auto_1fr] gap-2 mt-2.5">
+    
+                        <div class="flex justify-between items-center border-2 border-gray-200 rounded-4xl">
+                            <button class="px-3 py-1 text-gray-600 hover:text-gray-900 ">-</button>
+                            <span class="px-3 py-1 font-semibold">1</span>
+                            <button class="px-3 py-1 text-gray-600 hover:text-gray-900">+</button>
+                        </div>
+    
+                        <div class="">
+                            <button class="w-full bg-amber-600 text-center p-1.5 rounded-4xl text-white ">Add to cart</button>
+                        </div>
+                        </div>
+    
+                    </div>
+    
+                    </div> 
+                    
+                </div>
+                `
+                appn.after(detailsele);
+    
+            }
         }
     }
 
 }
+
 
 
 
--- src/lesPlatisponible.js
+++ src/lesPlatisponible.js
@@ -12,48 +12,45 @@ function fetcheApi() {
             // console.log(data.dishes.length);
             data.dishes.forEach(ele => {
                 div = document.createElement("div");
-                div.setAttribute('class',"oneCard") ;
-                div.setAttribute('data-id',ele.id) ;
-                // div.cssText="w-60 h-80  shadow-2xl rounded-\[8px\] flex flex-col  items-center justify-evenly";
+                div.setAttribute('class', "oneCard");
+                // div.setAttribute('data-id', ele.id);
+                // div.cssText="w-60 h-80  shadow-2xl rounded-[8px] flex flex-col  items-center justify-evenly";
                 div.setAttribute('class', 'w-60 h-80  rounded-[8px] flex flex-col  items-center justify-evenly shadow-[0px_5px_15px_rgba(0, 0, 0, 0.35)] ');
                 // console.log(ele.image);
 
                 div.innerHTML = `
-             <img src="../${ele.image}" class="w-45 oneCardImgPlat" alt="none">
-            <div class="titleAndPrice w-46 flex justify-between">
-                <h3 class="inter-font font-semibold text-[15px] titlePlat">${ele.name}</h3>
-                <h4 class="inter-font font-semibold PricePlat"><span class="text-[#FF6868] text-[12px]">$</span>${ele.price}</h4>
-            </div>
-            <p class="inter-font text-[#555555] text-[13px] descriptionPlat  w-[10rem]">${ele.description}</p>
-            <div class="sizeAndBtnAddToCart w-[13rem] flex  justify-between items-center ">
-                <div class="flex w-1/2 justify-between sizesPlat">
-                    <div class="S  bg-black text-white w-7 h-7 text-center rounded-[5px] smalSize">S</div>
-                    <div class="M  bg-white text-black w-7 h-7 text-center rounded-[5px] border meduimSize">M</div>
-                    <div class="L bg-white text-black w-7 h-7 text-center rounded-[5px] border largSize">L</div>
-                </div>
-                <button class="btnAddToCart w-7/12 h-8 bg-[#F59124] rounded-[8px] ml-2 text-white">+ To Cart</button>
-            </div>
-            `;
+                    <img src="../${ele.image}" class="w-45 oneCardImgPlat" alt="none">
+                    <div class="titleAndPrice w-52 flex justify-between">
+                        <h3 class="inter-font font-semibold text-[15px] cursor-pointer pourDetails"  data-id="${ele.id}" data-categhorie="dishes"  titlePlat">${ele.name}</h3>
+                        <h4 class="inter-font font-semibold PricePlat"><span class="text-[#FF6868] text-[12px]">$</span>${ele.price}</h4>
+                    </div>
+                    <p class="inter-font text-[#555555] text-[13px] descriptionPlat  w-[13rem]">${ele.description}</p>
+                    <div class="sizeAndBtnAddToCart w-[13rem] flex  justify-between items-center ">
+                        <div class="flex w-1/2 justify-between sizesPlat">
+                            <div class="S  bg-black text-white w-7 h-7 text-center rounded-[5px] smalSize">S</div>
+                            <div class="M  bg-white text-black w-7 h-7 text-center rounded-[5px] border meduimSize">M</div>
+                            <div class="L bg-white text-black w-7 h-7 text-center rounded-[5px] border largSize">L</div>
+                        </div>
+                        <button class="btnAddToCart w-7/12 h-8 bg-[#F59124] rounded-[8px] ml-2 text-white">+ To Cart</button>
+                    </div>`
+                    ;
                 sectionCards.appendChild(div);
             })
+
+            document.querySelectorAll(".pourDetails").forEach(el => {
+                el.addEventListener("click" , (e) => {
+
+                    const id = e.currentTarget.dataset.id;
+                    const categorie = e.currentTarget.dataset.categhorie;
+
+                    localStorage.setItem("courrentcategorieDetails" , categorie );
+                    localStorage.setItem("courrentIdDetails" , id );
+
+                    location.href = "/src/pages/detail-menu.html";
+
+                })
+            });
         }
 
         )
 }
-
-{/* <div class="oneCard w-60 h-80  shadow-2xl rounded-\[8px\] flex flex-col  items-center justify-evenly">
-            <img src="../images/Plat.svg" class="w-40 oneCardImgPlat" alt="none">
-            <div class="titleAndPrice w-56 flex justify-around">
-                <h3 class="inter-font font-semibold text-[17px] titlePlat">Egg vegi salad</h3>
-                <h4 class="inter-font font-semibold PricePlat"><span class="text-[#FF6868] text-[12px]">$</span>23.00</h4>
-            </div>
-            <p class="-ml-14 inter-font text-[#555555] text-[13px] descriptionPlat">Description of the item</p>
-            <div class="sizeAndBtnAddToCart w-\[13rem] flex  justify-between items-center ">
-                <div class="flex w-1/2 justify-between sizesPlat">
-                    <div class="S  bg-black text-white w-7 h-7 text-center rounded-[5px] smalSize">S</div>
-                    <div class="M  bg-white text-black w-7 h-7 text-center rounded-[5px] border meduimSize">M</div>
-                    <div class="L bg-white text-black w-7 h-7 text-center rounded-[5px] border largSize">L</div>
-                </div>
-                <button class="btnAddToCart w-7/12 h-8 bg-[#F59124] rounded-\[8px] ml-2 text-white">+ To Cart</button>
-            </div>
-        </div> */}
